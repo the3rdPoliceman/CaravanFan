3d85d48b5ab3901472321386f579447a269bfb65
dave <gabbymoore@gmail.com>
2023-06-22 18:06:39 +0200

    changed default temperature in mockDHT

--- mockDHT.py
+++ mockDHT.py
@@ -2,4 +2,4 @@ DHT22 = 'DHT22'
 
 def read_retry(sensor, pin):
     print(f'Reading sensor {sensor} on pin {pin}')
-    return 50, 20  # Returns a mock humidity and temperature
+    return 50, 40  # Returns a mock humidity and temperature
